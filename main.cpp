ffb74abec75834aaace8bd79aa764e8d1f6aa75e
KandleM6 <77935154+KandleM6@users.noreply.github.com>
2021-05-01 20:50:30 -0500

    Hola

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
-
+#include <string>
 int main(){
 	std::cout<<"Hola mundo";
+	std::string s = "Hola";
+	std::cout<<std::endl;
+	for (int i=0;i<4;i++){
+		std::cout << s[i];
+	}
 	return 0;
 }
